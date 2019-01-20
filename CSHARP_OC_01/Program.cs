fded1b3a29a2dbe1d45cb9755292d06e66aa12ea
Clément <clement.chancerel@outlook.fr>
2019-01-20 14:28:31 +0100

    Programme console déterminant si l'on est en semaine ou en weekend, ainsi que la journée ou le soir.

--- CSHARP_OC_01/Program.cs
+++ CSHARP_OC_01/Program.cs
@@ -4,9 +4,32 @@ namespace CSHARP_OC_01
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string name = Environment.UserName;
+
+            string day = DateTime.Now.DayOfWeek.ToString();
+
+            int hour = DateTime.Now.Hour;
+
+            if (day == "Saturday" || day == "Sunday")
+            {
+                Console.WriteLine("Bon weekend " + name + " !");
+
+            } else
+            {
+                if (hour > 9 || hour < 18)
+                {
+                    Console.Write("Bonjour " + name + ".");
+                }
+                else if (hour > 18 || hour < 9)
+                {
+                    Console.WriteLine("Bonsoir " + name + ".");
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
